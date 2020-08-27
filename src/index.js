f045257496c40ed80a3c5c6f3f6ac40a2630366d
sharatramaraju <sharat1000@gmail.com>
2020-08-27 15:01:16 -0500

    Promise

--- src/index.js
+++ src/index.js
@@ -9,23 +9,46 @@ document.getElementById("app").innerHTML = `
 </div>
 `;
 
-console.log(getSomeDataSynchronously());
+// console.log(getSomeDataSynchronously());
 
-getSomeDataAsynchronously(errorHandler, resultHandler);
-function resultHandler(data) {
-  console.log("response is ", data);
-}
-function errorHandler(error) {
-  console.log(error.message);
-}
+// getSomeDataAsynchronously(errorHandler, resultHandler);
+// function resultHandler(data) {
+//   console.log("response is ", data);
+// }
+// function errorHandler(error) {
+//   console.log(error.message);
+// }
 
-function getSomeDataSynchronously() {
-  return "Sync Data";
-}
+// function getSomeDataSynchronously() {
+//   return "Sync Data";
+// }
 
-function getSomeDataAsynchronously(eh, cb) {
-  setTimeout(function returnThatData() {
-    //return "ASync Data";
-    cb("async data");
-  }, 3000);
+// function getSomeDataAsynchronously(eh, cb) {
+//   setTimeout(function returnThatData() {
+//     //return "ASync Data";
+//     cb("async data");
+//   }, 3000);
+// }
+
+getDataUsingPromise().then(
+  (data) => {
+    console.log(data);
+  },
+  (rejectReason) => {
+    console.log(rejectReason);
+  }
+);
+
+function getDataUsingPromise() {
+  console.log("Promise");
+  const shouldReject = Math.round(Math.random());
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldReject) {
+        reject("Promize could not be fulfilled");
+      } else {
+        resolve("Promize rezolved!");
+      }
+    }, 1000);
+  });
 }

7da19d8c39f797227a67f4c9e5d4062ddbd9c4d8
sharatramaraju <sharat1000@gmail.com>
2020-08-27 14:17:52 -0500

    Callback

--- src/index.js
+++ src/index.js
@@ -8,3 +8,24 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+console.log(getSomeDataSynchronously());
+
+getSomeDataAsynchronously(errorHandler, resultHandler);
+function resultHandler(data) {
+  console.log("response is ", data);
+}
+function errorHandler(error) {
+  console.log(error.message);
+}
+
+function getSomeDataSynchronously() {
+  return "Sync Data";
+}
+
+function getSomeDataAsynchronously(eh, cb) {
+  setTimeout(function returnThatData() {
+    //return "ASync Data";
+    cb("async data");
+  }, 3000);
+}
